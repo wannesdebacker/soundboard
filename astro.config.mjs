19cccbf8cae26685c8616ce1ba29960620c56b5f
Wannes De Backer <debacker.wannes@gmail.com>
2025-11-28 15:48:14 +0100

    deploy test

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,8 @@ import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs()]
+  site: 'https://wannesdebacker.github.io',
+  base: '/soundboard',
+  output: 'static',
+  integrations: [solidJs()],
 });
