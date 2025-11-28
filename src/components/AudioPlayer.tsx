3cb6a9d85a12b0bde0d78377adb4cba322e5e531
Wannes De Backer <debacker.wannes@gmail.com>
2025-11-28 15:37:39 +0100

    Update sounds

--- src/components/AudioPlayer.tsx
+++ src/components/AudioPlayer.tsx
@@ -1,40 +1,40 @@
-import type { JSX } from 'solid-js';
-import styles from './AudioPlayer.module.css';
+import type { JSX } from "solid-js";
+import styles from "./AudioPlayer.module.css";
 
 interface AudioPlayerProps {
-    title: string;
-    src: string;
-    type?: string;
+  title: string;
+  src: string;
+  type?: string;
 }
 
 type Component<P = {}> = (props: P) => JSX.Element;
 
 const AudioPlayer: Component<AudioPlayerProps> = (props) => {
-    let audioRef: HTMLAudioElement;
+  let audioRef: HTMLAudioElement;
 
-    const playAudio = () => {
-        document.querySelectorAll('audio').forEach(audio => {
-            if (audio !== audioRef) {
-                audio.pause();
-                audio.currentTime = 0;
-            }
-        });
+  const playAudio = () => {
+    document.querySelectorAll("audio").forEach((audio) => {
+      if (audio !== audioRef) {
+        audio.pause();
+        audio.currentTime = 0;
+      }
+    });
 
-        audioRef.currentTime = 0;
-        audioRef.play();
-    };
+    audioRef.currentTime = 0;
+    audioRef.play();
+  };
 
-    return (
-        <div class="audio-player">
-            <button onClick={playAudio} class={styles['audio-button']} type="button">
-                <span class={styles['audio-button__a11y-text']}>Play sound:</span>
-                <span class={styles['audio-button__text']}>{props.title}</span>
-            </button>
-            <audio ref={audioRef} preload="none">
-                <source src={props.src} type={props.type || 'audio/mpeg'} />
-            </audio>
-        </div>
-    );
-}
+  return (
+    <div class="audio-player">
+      <button onClick={playAudio} class={styles["audio-button"]} type="button">
+        <span class={styles["audio-button__a11y-text"]}>Play sound:</span>
+        <span class={styles["audio-button__text"]}>{props.title}</span>
+      </button>
+      <audio ref={audioRef} preload="none">
+        <source src={props.src} type={props.type || "audio/mpeg"} />
+      </audio>
+    </div>
+  );
+};
 
 export default AudioPlayer;
